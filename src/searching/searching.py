9ff43a61d2c46e2a20db5c628fb5713eb300f5b5
Justin Russell <Justin@justins-macbook.lan>
2020-08-18 12:04:05 -0700

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,21 @@
 # TO-DO: Implement a recursive implementation of binary search
+import math
+
+
 def binary_search(arr, target, start, end):
     # Your code here
+    if len(arr) == 0:
+        return -1
+    mid = math.floor((start + end)/2)
+    if arr[mid] == target:
+        return mid
+    elif start >= end:
+        return -1
+    else:
+        if target > arr[mid]:
+            return binary_search(arr, target, mid + 1, end)
+        else:
+            return binary_search(arr, target, start, mid - 1)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +24,6 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
 
+# def agnostic_binary_search(arr, target):
+#     # Your code here
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,13 +4,35 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-
-
+    i = 0
+    a = 0
+    b = 0
+    while i < elements:
+        if a >= len(arrA):
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            if arrA[a] < arrB[b]:
+                merged_arr[i] = arrA[a]
+                a += 1
+            else:
+                merged_arr[i] = arrB[b]
+                b += 1
+        i += 1
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+import math
 def merge_sort(arr):
     # Your code here
+    if len(arr) < 2:
+        return arr
+    else:
+        mid = math.floor(len(arr) / 2)
+        return merge(merge_sort(arr[:mid]), merge_sort(arr[mid:]))
 
 
     return arr
@@ -19,10 +41,10 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
